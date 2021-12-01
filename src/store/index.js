72441e5298b7af688385b4c07a0cdd1e28989857
richardingwe <richardingwe5@gmail.com>
2021-12-01 05:10:34 +0100

    created a mutation to update state.

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,11 @@ import { createStore } from 'vuex';
 const store = createStore({
     state: {
         points: 0
+    },
+    mutations: {
+        updatePoints(state, payload) {
+            state.points = state.points + payload;
+        }
     }
 });
 
